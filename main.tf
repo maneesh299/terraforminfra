0450591472e04ea5ea841d2178262b52ec43511a
Maneesh Murali <maneesh299@gmail.com>
2023-06-19 09:05:07 +0000

    changing gke cluster code

--- main.tf
+++ main.tf
@@ -7,6 +7,23 @@ resource "google_container_cluster" "gke-cluster" {
   remove_default_node_pool = true
   initial_node_count = 1
 }
+resource "google_container_node_pool" "gke-node-pool" {
+  name = "sonarqube"
+  location = "asia-southeast1"
+  cluster = google_container_cluster.gke-cluster.name
+  node_count = 1
+
+  node_config {
+    machine_type = "n1-standard-1"
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    labels = {
+      app = "sonarqube"
+    }
+  }
+}
 resource "google_compute_disk" "my_disk" {
   name  = "my-gce-pd-volume"
   size  = 10
