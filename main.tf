bb6f8b85ed1383bed399f32638ebec484f58c095
Maneesh Murali <maneesh299@gmail.com>
2023-06-19 08:46:35 +0000

    new gke code

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
-resource "google_container_cluster" "pythonetl-cluster" {
-  name     = "pythonetl-cluster"
-  location = var.zone
-
-  remove_default_node_pool = true
-}
-
-resource "google_container_node_pool" "default" {
-  name       = "default"
-  cluster    = google_container_cluster.pythonetl-cluster.name
-  location   = google_container_cluster.pythonetl-cluster.location
-  node_count = 1
+resource "google_container_cluster" "my_cluster" {
+  name               = "my-cluster"
+  location           = "europe-west2"
+  initial_node_count = 1
+  release_channel    = "regular"
+  network            = "projects/eng-district-390211/global/networks/default"
+  subnetwork         = "projects/eng-district-390211/regions/europe-west2/subnetworks/default"
+  cluster_ipv4_cidr  = "/17"
+  services_ipv4_cidr = "/22"
 }
 resource "google_compute_disk" "my_disk" {
   name  = "my-gce-pd-volume"
